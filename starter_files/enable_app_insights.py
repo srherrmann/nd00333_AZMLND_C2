6c028783bc1efd4485aa1bf65133e6b9a612f3ac
Stephan Herrmann <st.herrmann1@gmail.com>
2025-01-16 19:22:23 +0100

    Project Step 3: enable application insights

---
+++ starter_files/enable_app_insights.py
@@ -0,0 +1,8 @@
+from azureml.core import Workspace
+from azureml.core.webservice import Webservice
+
+# Requires the config to be downloaded first to the current working directory
+ws = Workspace.from_config()
+name = "bankmarketing-deployment"
+service = Webservice(name=name, workspace=ws)
+service.update(enable_app_insights=True)
