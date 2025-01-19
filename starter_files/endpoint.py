0dc4e34c1d534df80f1fa033308a1b9e2f353673
Stephan Herrmann <st.herrmann1@gmail.com>
2025-01-19 14:15:44 +0100

    endpoint notebook as shown in the video

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,12 +3,14 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://c602dfc5-704b-44f3-a9fd-9b64d9618380.germanywestcentral.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = '1TpQXBrhU2bucHTgfsAtLHjOiiSNfOFB'
 
 # Two sets of data to score, so we get two results back
-data = {"data":
+data = {
+    "Inputs": {
+        "data":
         [
           {
             "age": 17,
@@ -55,6 +57,7 @@ data = {"data":
             "previous": 1
           },
       ]
+    }
     }
 # Convert to JSON string
 input_data = json.dumps(data)
